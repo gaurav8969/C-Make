92dde2556ab0e5e2ee9ef64a35c674c3c3ff89dd
Gaurav Tiwari <gauravkrtiwari79@gmail.com>
2022-11-19 19:56:27 +0530

    updated the program to remove the  problem causing c++20 features for now, need to get the latest version of c++

--- 1st project/source/main.cpp
+++ 1st project/source/main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
-#include <concepts>
-
-template <typename T>
-requires std::integral<T>
-T add( T a, T b){
-    return a + b;
-}
 
 int main(){
-    std::cout << "Hello from C++ 20 with CMake on Linux/Windows" << std::endl;
-    std::cout << "The sun is : " << add(7,10) << std::endl;
+    std::cout << "Hello from C++ 17 with CMake on Linux/Windows" << std::endl;
     return 0;
 }
